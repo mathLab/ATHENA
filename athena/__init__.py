6ceffdd408d2c93774e207ecd4f798c23c0578b5
mtezzele <marcotez@gmail.com>
2020-07-07 14:02:33 +0200

    init

--- athena/__init__.py
+++ athena/__init__.py
@@ -14,7 +14,6 @@ __mail__ = 'marcotez@gmail.com, francesco.romor@gmail.com'
 __maintainer__ = __author__
 __status__ = "Beta"
 
-
 from .active import ActiveSubspaces
 from .feature_map import (FeatureMap, rff_map, rff_jac)
 from .kas import KernelActiveSubspaces
